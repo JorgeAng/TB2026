b3a1d47f43eabafcc38947273975b3b2720bdabd
JorgeAng <132932314+JorgeAng@users.noreply.github.com>
2026-02-02 15:35:28 -0600

    Add option for 48 in select dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -926,6 +926,7 @@ const QuoteEditor = () => {
                     <option value={12}>12"</option>
                     <option value={16}>16"</option>
                     <option value={24}>24"</option>
+                    <option value={48}>48"</option>
                   </select>
                 </div>
                 <div>
